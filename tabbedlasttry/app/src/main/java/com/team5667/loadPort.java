9bbb658a3c3e2441e6f26a17bf45e0f41f383666
cperryoh <46071730+cperryoh@users.noreply.github.com>
2020-03-07 17:11:42 -0500

    image stuff

--- tabbedlasttry/app/src/main/java/com/team5667/loadPort.java
+++ tabbedlasttry/app/src/main/java/com/team5667/loadPort.java
@@ -1,14 +1,21 @@
 package com.team5667;
 
+import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.text.InputType;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -20,6 +27,7 @@ import java.util.ArrayList;
 public class loadPort extends tab {
     match parent;
     timer run;
+    ImageView port;
 
     public int total;
     private final int maxTime;
@@ -84,11 +92,34 @@ public class loadPort extends tab {
         }
     }
 
+    @SuppressLint("ClickableViewAccessibility")
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(layout, container, false);
         parent = (match) getParentFragment().getParentFragment();
+        port=view.findViewById(R.id.loadPort);
+        port.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                int test = event.getPointerCount();
+                int x = (int) event.getRawX();
+                int y = (int) event.getRawY();
+                int[] viewCoords = new int[2];
+                port.getLocationInWindow(viewCoords);
+                int imageX = x- viewCoords[0]; // viewCoords[0] is the X coordinate
+                int imageY =y- viewCoords[1];
+                Drawable draw = getResources().getDrawable(R.drawable.map);
+                Bitmap bitmap = ((BitmapDrawable)draw).getBitmap();
+                int pixel = bitmap.getPixel(imageX,imageY);
+                int redValue = Color.red(pixel);
+                int blueValue = Color.blue(pixel);
+                int greenValue = Color.green(pixel);
+                Toast.makeText(getContext(),redValue+","+greenValue+","+blueValue,Toast.LENGTH_SHORT).show();
+
+                return false;
+            }
+        });
         activity = (MainActivity) getActivity();
         //fetches all the buttons
         Button one = view.findViewById(R.id.LevelOne);
